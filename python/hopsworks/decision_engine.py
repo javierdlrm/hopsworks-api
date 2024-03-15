981cf9cf4284927422053d7442463df3cfbffd19
Kateryna Herashchenko <kategera6@gmail.com>
2024-03-15 02:41:58 +0100

    fix

--- python/hopsworks/decision_engine.py
+++ python/hopsworks/decision_engine.py
@@ -669,7 +669,7 @@ class ItemCatalogEmbedding(tf.keras.Model):
                 reshaped_tensor = tf.reshape(
                         self.normalized_feats[feat](numeric_inputs[feat]), (-1, 1)
                     )
-                if tf.rank(layers[0]).numpy() == 3 and tf.rank(reshaped_tensor).numpy() == 2:
+                if len(layers[0].shape) == 3 and len(reshaped_tensor.shape) == 2:
                     reshaped_tensor = tf.expand_dims(reshaped_tensor, axis=0)
                 layers.append(reshaped_tensor)
                 
